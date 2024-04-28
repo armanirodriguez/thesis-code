62c5cae1e6ac3c9287e0482ec44e17c45beb5a9a
Armani <69732521+armanirodriguez@users.noreply.github.com>
2024-04-28 16:40:25 -0400

    Delete resnet_experiment.py
    Unneeded

--- resnet_experiment.py
+++
@@ -1,8 +0,0 @@
-import torch
-from resnet50 import ResNet50
-
-CLASSIFIER_IMAGENET_SAVE_PATH = './checkpoints/resnet_imagenet.pt'
-
-if __name__ == '__main__':
-    classifier = torch.load(CLASSIFIER_IMAGENET_SAVE_PATH)
-    print(classifier)
