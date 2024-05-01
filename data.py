400c8aa49865fb8edd7ab143b0c2e7ec717ee07c
Armani Rodriguez <rodrig52@stuents.rowan.edu>
2024-05-01 17:32:28 -0400

    jpeg comparison

--- data.py
+++ data.py
@@ -2,11 +2,14 @@
 Various utility functions for data processing and analysis
 """
 import torch
+import simplejpeg
+import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.datasets import MNIST
 from ImageNet100 import ImageNet100
 from tqdm import tqdm
+
 
 def get_mnist_dataloaders():
     transform = transforms.Compose(
@@ -55,3 +58,25 @@ def get_preds(classifier, dataloader, transform=None):
     Y = list(map(lambda y : y.item(), Y))
     Y_pred = list(map(lambda y : y.item(), Y_pred))
     return Y, Y_pred
+
+import simplejpeg
+
+def reconstructWithJPEG(tensor, quality=100, return_ratio = False):
+    """
+    Reconstruct a tensor representing a batch of images tensor JPEG
+
+    return_ratio: returns the compression ratio if True
+    quality: The quality of the reconstruction [0-100]
+    """
+    recs = []
+    for img in tensor:
+        colorspace = "GRAY" if img.shape[0] == 1 else "RGB"
+        img = img.numpy()
+        img = (255 * img).astype(np.uint8)
+        img = np.ascontiguousarray(img.swapaxes(0,2))
+        jpeg = simplejpeg.encode_jpeg(img, colorspace=colorspace, quality=quality)
+        dec = simplejpeg.decode_jpeg(jpeg, colorspace=colorspace).swapaxes(0,2)
+        recs.append(dec)
+    if return_ratio:
+        return torch.tensor(recs, dtype=torch.float32) / 255., np.prod(img.shape)/ len(jpeg)
+    return torch.tensor(recs, dtype=torch.float32) / 255
